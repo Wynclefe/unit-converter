679c81d879cdbfd64bedb21b2b13c58c6684883b
wynclefe <wynclefe@gmail.com>
2022-10-12 13:13:36 +0800

    Create conversion for meter to feet and vice versa

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ let volumeResult = document.getElementById("volume-result")
 let massResult = document.getElementById("masss-result")
 
 convertBtn.addEventListener("click", function (){
-        let meterToFeet = `${inputEl.value} meters = ${inputEl.value * 3.281} meters`
+        let meterToFeet = `${inputEl.value} meters = ${inputEl.value * 3.281} meters | ${inputEl.value} feet = ${(inputEl.value / 3.281).toFixed(3)} feet`
+        meterResult.textContent = meterToFeet
         console.log(meterToFeet)
+      
     })
     
 
