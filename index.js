72a2e0a30521e155bf353a91adbe0bc0534c8586
wynclefe <wynclefe@gmail.com>
2022-10-12 13:20:24 +0800

    Change let variables into const

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const convertBtn = document.getElementById("convert-btn")
 
 // variables in the results sections
 
-let meterResult = document.getElementById("meter-result")
-let volumeResult = document.getElementById("volume-result")
-let massResult = document.getElementById("masss-result")
+const meterResult = document.getElementById("meter-result")
+const volumeResult = document.getElementById("volume-result")
+const massResult = document.getElementById("masss-result")
 
 convertBtn.addEventListener("click", function (){
-        let meterToFeet = `${inputEl.value} meters = ${inputEl.value * 3.281} meters | ${inputEl.value} feet = ${(inputEl.value / 3.281).toFixed(3)} feet`
+        const meterToFeet = `${inputEl.value} meters = ${inputEl.value * 3.281} meters | ${inputEl.value} feet = ${(inputEl.value / 3.281).toFixed(3)} feet`
         meterResult.textContent = meterToFeet
         console.log(meterToFeet)
       
