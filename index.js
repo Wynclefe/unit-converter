85426cc9746ba8a5c296315a2ba7369cb1ab3021
wynclefe <wynclefe@gmail.com>
2022-10-12 12:32:48 +0800

    Add render function with convert-btn addEventListener

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 
 const inputEl = document.getElementById("input-el")
 const convertBtn = document.getElementById("convert-btn")
+
+// variables in the results sections
+
+let meterResult = document.getElementById("meter-result")
+let volumeResult = document.getElementById("volume-result")
+let massResult = document.getElementById("masss-result")
+
+function renderResult() {
+    convertBtn.addEventListener("click", function (){
+    })
+}
+
